a15e446075a87a2348322c5f94e5d127233e7212
Simran-0024 <simranthecoder24@gmail.com>
2024-10-14 23:11:24 +0530

    completed

---
+++ app.js
@@ -0,0 +1,99 @@
+let userScore=0;
+let compScore=0;
+
+const choices = document.querySelectorAll(".moves");
+const uScore=document.querySelector("#user-score"); 
+const cScore=document.querySelector("#comp-score"); 
+
+
+const genComp=()=>{
+    const options=["rock","Paper","Scissors"];
+    const ranIndx = Math.floor(Math.random() * 3);
+    return options[ranIndx];
+
+}
+const Draw=()=>{
+    console.log("It's a tie!");
+}
+const checkScore=(userWin)=>{
+    if(userWin===true){
+        userScore++;
+        uScore.innerText=userScore;
+
+        
+    }
+    else{
+        compScore++;
+        cScore.innerText=compScore;
+
+    }
+    
+}
+const playGame=(userChoice)=>{
+    console.log(userChoice);
+    const compChoice = genComp();
+    console.log(compChoice);
+
+    if(userChoice === compChoice){
+        Draw();
+    }
+    else{
+        let userWin=true;
+        if (userChoice === "rock"){
+            // now computers choice can be either scissprs or paper
+            if(compChoice==="Scissors"){
+                userWin=true;
+
+                
+            }
+            else{
+                userWin=false;
+            }
+
+            
+        }
+        else if(userChoice === "paper"){
+            // now computers choice can be either rock or scissors
+            if(compChoice==="Rock"){
+                userWin=true;
+            }
+            else{
+                userWin=false;
+            }
+           
+
+        }
+        else {
+
+            if (userChoice === "Scissors"){
+                // now computers choice can be either rock or paper
+                if(compChoice==="Paper"){
+                    userWin=true;
+                }
+                else{
+                    userWin=false;
+                }
+           
+
+            }
+            
+
+ 
+        }
+        checkScore(userWin);
+    }
+
+
+
+
+}
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+
+        
+
+    });
+
+})   
